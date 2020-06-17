5dadbd6030cac5e01204e2a8428f402f0decc313
pixelzerg <pixelzerg@gmail.com>
2020-06-17 19:10:26 +0100

    begin work on regex

--- lang/lexer.py
+++ lang/lexer.py
@@ -1,23 +1,17 @@
-class Part:
-    def __init__(self, part_txt=None):
-        self.lhs = None
-        self.operator = None
-        self.rhs = None
+import re
 
-        if part_txt is not None:
-            self.__parse(part_txt)
-
-    def __parse(self, part_txt):
-        pass
+REGEX = r"(\w+)" \
+        r"(?:(" \
+            r">=|<=|=|!=|>|<" \
+        r")(" \
+            r"\w+|" \
+            r"\"(?:\\.|[^\"\\\n])*)\"" \
+        r")?"
 
 def lex(exp: str):
-    for part_txt in exp.split(' '):
-        if len(part_txt) <= 0:
-            continue
-        yield __lex_part(part_txt)
+    for match in re.finditer(REGEX, exp):
+        print(match.group(0))
 
-def __lex_part(sub_exp: str):
-    print(sub_exp)
-
-if __name__ == '__main__':
-    list(lex("art  time<=yesterday genre=roc% rating>3"))
+# if __name__ == '__main__':
+#     list(lex("art  time<=yesterday genre=roc% rating>3"))
+print(REGEX)
